a74956ef8e3df2eef23423b5293ea391e74374ae
gilianetdy <71667808+gilianetdy@users.noreply.github.com>
2020-10-08 16:33:33 +0200

    Add files via upload

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -4,37 +4,37 @@ require 'rubygems'
 require 'nokogiri' 
 require 'open-uri'
 
-puts "data is coming..."
+puts "Data is coming..."
 
-#1 Première méthode : Déclaration de la page à scrapper
+#1 Déclaration de la page à scrapper
 def get_page
 	page = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all/"))
 	return page
 end
 
-#2 Deuxième méthode : Collecte des abbréviations des noms des monnaies
+#2 Collecte des abbréviations des noms des monnaies
 def scrapp_symbols
 	page = get_page
 	symbols = page.xpath('//*[@id="__next"]/div/div[2]/div[1]/div[2]/div/div[2]/div[3]/div/table/tbody/tr/td[3]/div')
-	symbols_array = [] #/ initialisation d'un array pour les stocker
+	symbols_array = [] # Array pour les stocker
 	symbols.each do |symbol| 
-		symbols_array << symbol.text #.text pour avoir les infos en string
+		symbols_array << symbol.text #.text pour récupérer le texte et << pour ajouter les valeurs à l'array
 	end
     return symbols_array
 end
 
-#3 Troisième méthode : Collecte des cours des monnaies
+#3 Collecte des cours des monnaies
 def scrapp_prices
 	page = get_page
 	prices = page.xpath('//*[@id="__next"]/div/div[2]/div[1]/div[2]/div/div[2]/div[3]/div/table/tbody/tr/td[5]/a')
 	prices_array = []
 	prices.each do |price|
-		prices_array << price.text[1..-1].to_f
+		prices_array << price.content.delete('$,').to_f #.text pour récupérer le texte du second caractère (pas de signe) au dernier - puis pour convertir en float 
 	end
     return prices_array
 end 
 
-#4 Quatrième méthode : Synchronisation des noms et des cours des monnaies
+#4 Synchronisation des noms et des cours des monnaies
 def darktrader_master
 	symbols_array = scrapp_symbols #/ on rappelle nos méthodes
 	prices_array = scrapp_prices 
--- lib/mairie_christmas.rb
+++ lib/mairie_christmas.rb
@@ -7,34 +7,34 @@ require 'open-uri'
 puts "Mairie du Val d'Oise, here we go..."
 
 
-#1 Première méthode : Récupération de l'email d'une mairie d'une ville du Val d'Oise
+#1 Récupération de l'email d'une mairie d'une ville du Val d'Oise
 def get_townhall_email(townhall_url)
-	page = Nokogiri::HTML(URI.open(townhall_url)) #/ on indique un site URL neutre qui sera indiqué dans la prochaine méthode
+	page = Nokogiri::HTML(URI.open(townhall_url)) #on indique un site URL neutre qui sera indiqué dans la prochaine méthode
 	email_array = []
 
 	email = page.xpath('//*[contains(text(), "@")]').text
-	town = page.xpath('//*[contains(text(), "Adresse mairie de")]').text.split #/ on divise la string pour pouvoir récupérer uniquement le nom de la ville
+	town = page.xpath('//*[contains(text(), "Adresse mairie de")]').text.split #on divise la string pour pouvoir récupérer uniquement le nom de la ville
 
-	email_array << {town[3] => email} #/ on indique la position du nom de la ville dans la string pour la récupérer
+	email_array << {town[3] => email} #on indique la position du nom de la ville dans la string pour la récupérer et => aide à séparer les valeurs 
 	puts email_array
 	return email_array
 end
 
-#2 Deuxième méthode : Récupération des URLs des villes du Val d'Oise
+#2 Récupération des URLs des villes du Val d'Oise
 def get_townhall_urls
 	page = Nokogiri::HTML(URI.open("http://annuaire-des-mairies.com/val-d-oise.html"))
 	url_array = []
 
-	urls = page.xpath('//*[@class="lientxt"]/@href') #/ toutes les URLs appartiennent à la classe lientxt
+	urls = page.xpath('//*[@class="lientxt"]/@href') # toutes les URLs appartiennent à la classe lientxt
 
-	urls.each do |url| #/ pour chaque URLs récupérées, il faut leur indiquer l'url parent "http://annuaire-des-mairies.com"
+	urls.each do |url| #pour chaque URLs récupérées, il faut leur indiquer l'url parent "http://annuaire-des-mairies.com"
 		url = "http://annuaire-des-mairies.com" + url.text[1..-1] #/ A l'url parent, on ajoute les urls récupérées du deuxième caractère au dernier caractère, car on veut se débarasser du point devant.
 		url_array << url		
 	end
 	return url_array 
 end
 
-#3 Troisième méthode : Synchronisation des noms des villes et des emails des mairies
+#3 Synchronisation des noms des villes et des emails des mairies
 def town_mail
 	url_array = get_townhall_urls 
 	    url_array.each do |townhall_url| #/ pour chaque URL d'une ville du Val d'Oise, on associe l'adresse mail de la mairie
